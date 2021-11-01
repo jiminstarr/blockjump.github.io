ec9d67b3d99e257a9e743d70b765b9b72192cd48
jiminstarr <93383188+jiminstarr@users.noreply.github.com>
2021-11-01 10:00:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function jump(){
 }
 
 var checkDead = setInterval(function(){
-    var characterTop = (window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = (window.getComputedStyle(block).getPropertyValue("left"));
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>0 && characterTop>=130) {
         block.style.animation = "none"
         block.style.display = "none"
